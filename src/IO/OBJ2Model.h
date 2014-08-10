6949ff0c7bb18efb0265efb7d0ff0903e71a77a3
Juul <Joosten.Juul@gmail.com>
2014-08-09 20:42:51 -0700

    Cleanup of unused files.

--- src/IO/OBJ2Model.h
+++
@@ -1,9 +0,0 @@
-#ifndef CSR_2_MODEL_H
-#define CSR_2_MODEL_H
-
-#include "OBJLoader.h"
-#include "../VertexData/Mesh.h"
-
-Mesh* CreateMeshFromObjGroup( OBJObject* objObject, OBJGroup* objGroup);
-
-#endif
