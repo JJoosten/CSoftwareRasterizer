ae20ec711c6921f15c449bf018523278e85afff4
Juul <Joosten.Juul@gmail.com>
2014-08-15 20:33:29 -0700

    Fixed small error in screen space coordinate generation, forgot to go from NDC to screenspace

--- src/Renderer/Rasterizer.c
+++ src/Renderer/Rasterizer.c
@@ -1,10 +1,11 @@
 #include "Rasterizer.h"
 #include <assert.h>
 
-void calculateScreenCoords( Vec4* const IN_OUT_ndc)
+void calculateScreenCoords( Renderer* const renderer, Vec4* const IN_OUT_screenCoordinates)
 {
-	IN_OUT_ndc->X = 1.0f + IN_OUT_ndc->X * 0.5f;
-	IN_OUT_ndc->Y = 1.0f + IN_OUT_ndc->Y * 0.5f;
+	// clip coordinates -> NDC -> screen coordinates
+	IN_OUT_screenCoordinates->X = (1.0f + IN_OUT_screenCoordinates->X) * 0.5f * renderer->FrameBuffer->Width;
+	IN_OUT_screenCoordinates->Y = (1.0f + IN_OUT_screenCoordinates->Y) * 0.5f * renderer->FrameBuffer->Height;
 }
 
 void RasterizeTriangle( Renderer* const renderer, RasterTriangle* const triangle)
@@ -13,9 +14,9 @@ void RasterizeTriangle( Renderer* const renderer, RasterTriangle* const triangle
 	assert( triangle && "RasterizeTriangle triangle is NULL");
 
 	// calculate screen coords and store them in the xy of the triangle position
-	calculateScreenCoords( &triangle->V1.Position);
-	calculateScreenCoords( &triangle->V2.Position);
-	calculateScreenCoords( &triangle->V3.Position);
+	calculateScreenCoords( renderer,  &triangle->V1.Position);
+	calculateScreenCoords( renderer, &triangle->V2.Position);
+	calculateScreenCoords( renderer, &triangle->V3.Position);
 
 	// go over triangle edges
 
